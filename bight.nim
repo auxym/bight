5a15b09ca451929ca4e2b3884424da9b3059d241
Francis Therien <frtherien@gmail.com>
2023-05-01 21:18:05 -0400

    Add read procs for 16 bit ints

--- bight.nim
+++ bight.nim
@@ -1,3 +1,26 @@
+# MIT License
+#
+# Copyright (c) 2023 Francis Thérien
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 template toOpenArrayByte(p: pointer, len: Natural): untyped =
   cast[ptr UncheckedArray[byte]](p).toOpenArray(0, len - 1)
 
@@ -77,6 +100,32 @@ proc readI32BE*(b: openArray[byte]): int32 =
 proc readI32BE*(p: pointer): int32 =
   cast[int32](readU32BE(p))
 
+proc readU16LE*(b: openArray[byte]): uint16 =
+  const bmask = 255'u16
+  result = (bmask and b[1]) shl 8 or (bmask and b[0])
+
+proc readU16LE*(p: pointer): uint16 =
+  readU16LE(p.toOpenArrayByte(4))
+
+proc readI16LE*(b: openArray[byte]): int16 =
+  cast[int16](readU16LE(b))
+
+proc readI16LE*(p: pointer): int16 =
+  cast[int16](readU16LE(p))
+
+proc readU16BE*(b: openArray[byte]): uint16 =
+  const bmask = 255'u16
+  result = (bmask and b[0]) shl 8 or (bmask and b[1])
+
+proc readU16BE*(p: pointer): uint16 =
+  readU16BE(p.toOpenArrayByte(4))
+
+proc readI16BE*(b: openArray[byte]): int16 =
+  cast[int16](readU16BE(b))
+
+proc readI16BE*(p: pointer): int16 =
+  cast[int16](readU16BE(p))
+
 when isMainModule:
   assert readU64LE([0xFF'u8, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88]) == 9843086184167632639'u64
   assert readU64BE([0xFF'u8, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88]) == 18441921395520346504'u64
@@ -87,5 +136,10 @@ when isMainModule:
   assert readU32BE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == 4293844428'u32
   assert readI32LE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == -857870593'i32
   assert readI32BE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == -1122868'i32
+
+  assert readU16LE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == 61183'u16
+  assert readU16BE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == 65518'u16
+  assert readI16LE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == -4353'i16
+  assert readI16BE([0xFF'u8, 0xEE, 0xDD, 0xCC]) == -18'i16
 
   echo "OK"
